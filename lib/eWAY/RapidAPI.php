3b9f0b20b6f099e8f2ef27217f9e40a893409ef4
Fayland Lam <fayland@gmail.com>
2013-11-12 21:15:47 +0800

    add logo

--- lib/eWAY/RapidAPI.php
+++ lib/eWAY/RapidAPI.php
@@ -126,6 +126,7 @@ class RapidAPI {
         //curl_setopt($ch, CURLOPT_USERAGENT, "Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)");
         curl_setopt($ch, CURLOPT_TIMEOUT, 60);
         //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        // curl_setopt($ch, CURLOPT_VERBOSE, true);
         $response = curl_exec($ch);
 
         if (curl_errno($ch) != CURLE_OK) {
