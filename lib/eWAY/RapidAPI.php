074f71fc184f73ca8803960ac0f2c07cb3668b81
Fayland Lam <fayland@gmail.com>
2013-11-14 22:50:18 +0800

    verbose on 404 error

--- lib/eWAY/RapidAPI.php
+++ lib/eWAY/RapidAPI.php
@@ -4,6 +4,7 @@ namespace eWAY;
 /* check examples for usage */
 class RapidAPI {
     private $_url;
+    private $sandbox;
     private $username;
     private $password;
 
@@ -17,8 +18,10 @@ class RapidAPI {
 
         if (count($params) && isset($params['sandbox']) && $params['sandbox']) {
             $this->_url = 'https://api.sandbox.ewaypayments.com/';
+            $this->sandbox = true;
         } else {
             $this->_url = 'https://api.ewaypayments.com/';
+            $this->sandbox = false;
         }
     }
 
@@ -134,8 +137,9 @@ class RapidAPI {
             die();
         } else {
             $info = curl_getinfo($ch);
-            if ($info['http_code'] == 401) {
-                echo "<h2>Please check the API Key and Password</h2><pre>";
+            if ($info['http_code'] == 401 || $info['http_code'] == 404) {
+                $__is_in_sandbox = $this->sandbox ? ' (Sandbox)' : ' (Live)';
+                echo "<h2>Please check the API Key and Password $__is_in_sandbox</h2><pre>";
                 die();
             }
 
